2143784d03bdb980511f7d7b54ab0f12215f6a28
yankie <yakielarapacheco@yahoo.com>
2021-12-16 18:34:43 -0500

    Adding router component for routing in authenticated state

---
+++ src/routes/DashboardRouter.jsx
@@ -0,0 +1,24 @@
+import {Redirect, Route, Switch} from "react-router-dom";
+
+import {Navbar} from "../components/ui/Navbar";
+import {HeroScreen} from "../components/heroes/HeroScreen";
+import {DcScreen} from "../components/dc/DcScreen";
+import {MarvelScreen} from "../components/marvel/MarvelScreen";
+
+export const DashboardRouter = () => {
+    return(
+        <>
+            <Navbar />
+
+            <div>
+                <Switch>
+                    <Route exact path={"/marvel"} component={MarvelScreen} />
+                    <Route exact path={"/marvel/:id"} component={HeroScreen} />
+                    <Route exact path={"/dc"} component={DcScreen} />
+
+                    <Redirect to={'/marvel'} />
+                </Switch>
+            </div>
+        </>
+    )
+}
