f979d5eb86bfde09218354ff525b048bf691c8b9
yankie <yakielarapacheco@yahoo.com>
2021-12-16 19:17:36 -0500

    Refactoring component for put styles

--- src/routes/DashboardRouter.jsx
+++ src/routes/DashboardRouter.jsx
@@ -10,7 +10,7 @@ export const DashboardRouter = () => {
         <>
             <Navbar />
 
-            <div>
+            <div className="container mt-2">
                 <Switch>
                     <Route exact path={"/marvel"} component={MarvelScreen} />
                     <Route exact path={"/marvel/:id"} component={HeroScreen} />
