66c72b83db8c7b898b965fc73cbc0f3b69457ebe
yankie <yakielarapacheco@yahoo.com>
2021-12-23 11:20:44 -0500

    Adding authentication context provider

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,9 +1,18 @@
+import {useReducer} from "react";
 import {AppRouter} from "./routes/AppRouter";
+import {AuthContext} from "./auth/AuthContext";
+import {authReducer} from "./auth/authReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || {logged:false}
+}
 
 export const HeroesApp = () => {
+    const [user, dispatch] = useReducer(authReducer, {}, init)
+
   return(
-      <div>
+      <AuthContext.Provider value={{user, dispatch}}>
           <AppRouter />
-      </div>
+      </AuthContext.Provider>
   )
 }
