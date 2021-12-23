243142a59e034c467b6304abdd4c13dee4504615
yankie <yakielarapacheco@yahoo.com>
2021-12-23 12:02:34 -0500

    Refactoring style component

--- src/components/login/LoginScreen.jsx
+++ src/components/login/LoginScreen.jsx
@@ -20,8 +20,8 @@ export const LoginScreen = ({history}) => {
         <div className="container mt-5">
             <h1>Login</h1>
             <hr/>
-            
-            <button className="btn btn-primary" 
+
+            <button className="btn btn-primary"
                     onClick={handleLogin}>Login</button>
         </div>
     )
