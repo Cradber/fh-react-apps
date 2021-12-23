5b44f0e61d1c3f35d8fd1ad7dca8e744505aefec
yankie <yakielarapacheco@yahoo.com>
2021-12-23 12:00:59 -0500

    Adding user information to local storage

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,4 +1,4 @@
-import {useReducer} from "react";
+import {useEffect, useReducer} from "react";
 import {AppRouter} from "./routes/AppRouter";
 import {AuthContext} from "./auth/AuthContext";
 import {authReducer} from "./auth/authReducer";
@@ -9,6 +9,10 @@ const init = () => {
 
 export const HeroesApp = () => {
     const [user, dispatch] = useReducer(authReducer, {}, init)
+
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(user))
+    }, [user])
 
   return(
       <AuthContext.Provider value={{user, dispatch}}>
