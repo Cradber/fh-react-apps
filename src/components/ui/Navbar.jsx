8373ac0a38be17d5ffa2a0bec2f3a65c37bad06e
yankie <yakielarapacheco@yahoo.com>
2021-12-23 12:04:36 -0500

    Using auth context and route context for redirect in logout

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, {useContext} from 'react'
+import {Link, NavLink, useHistory} from 'react-router-dom'
+import {AuthContext} from "../../auth/AuthContext";
+import {types} from "../../types/types";
 
 export const Navbar = () => {
+    const {user: {name}, dispatch} = useContext(AuthContext)
+    const history = useHistory()
+
+    const handleLogout = () => {
+        history.replace('/login')
+        dispatch({type: types.logout})
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
 
