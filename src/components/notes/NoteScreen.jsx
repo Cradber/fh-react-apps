f37822cc75cd0c329b51c63849befa5680491bda
yankie <yakielarapacheco@yahoo.com>
2021-12-25 14:24:50 -0500

    Refactoring note component for edition

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -4,6 +4,21 @@ export const NoteScreen = () => {
   return (
       <div className="notes__main-content">
           <NoteAppBar />
+
+          <div className="notes__content">
+              <input className="notes__title-input"
+                     type="text"
+                     autoComplete="off"
+                     placeholder="Some awesome text" />
+
+              <textarea className="notes__textarea"
+                     placeholder="What happened today"/>
+
+              <div className="notes__images">
+                  <img src="https://composeclick.com/wp-content/uploads/2018/05/nature-1-1024x637.jpg"
+                       alt="note"/>
+              </div>
+          </div>
       </div>
   )
 }
