e4813448b3c7358f85e701ba33f110506b1a9057
yankie <yakielarapacheco@yahoo.com>
2021-12-25 12:29:21 -0500

    Adding note app bar component and its styles

---
+++ src/components/notes/NoteAppBar.jsx
@@ -0,0 +1,16 @@
+
+export const NoteAppBar = () => {
+  return (
+      <div className="notes__appbar">
+          <span>December 25, 2021</span>
+          <div>
+              <button className="btn">
+                  Picture
+              </button>
+              <button className="btn">
+                  Save
+              </button>
+          </div>
+      </div>
+  )
+}
