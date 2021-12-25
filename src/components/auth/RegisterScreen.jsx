bd665f6fabcfba06c0bfb200807bbff4a294fab5
yankie <yakielarapacheco@yahoo.com>
2021-12-25 11:03:32 -0500

    Adding register screen and its styles

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,8 +1,45 @@
+import {Link} from "react-router-dom";
 
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>Register Screen</h1>
-        </div>
+        <>
+            <h3 className="auth__title">Register</h3>
+
+            <form>
+                <input type="text"
+                       className="auth__input"
+                       placeholder="Name"
+                       name="name"
+                       autoComplete="off"/>
+
+                <input type="text"
+                       className="auth__input"
+                       placeholder="email"
+                       name="Email"
+                       autoComplete="off"/>
+
+                <input type="password"
+                       className="auth__input"
+                       placeholder="Password"
+                       name="password"/>
+
+                <input type="password"
+                       className="auth__input"
+                       placeholder="Confirm password"
+                       name="password2"/>
+
+
+                <button className="btn btn-primary btn-block"
+                        type="submit">
+                    Register
+                </button>
+
+                <Link to="/auth/login"
+                      className="link">
+                    Already registered?
+                </Link>
+
+            </form>
+        </>
     )
 }
