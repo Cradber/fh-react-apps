15c15a466b91162226320ccd18cb4087e4090441
yankie <yakielarapacheco@yahoo.com>
2021-12-25 12:04:43 -0500

    Refactoring journal screen component adding sidebar and skeleton main content

--- src/components/journal/JournalScreen.jsx
+++ src/components/journal/JournalScreen.jsx
@@ -1,8 +1,15 @@
+import {Sidebar} from "./Sidebar";
 
 export const JournalScreen = () => {
   return (
-      <div>
-          <h1>Journal Screen</h1>
+      <div className="journal__main-content">
+
+          <Sidebar />
+
+          <main>
+              <h1>Main content</h1>
+          </main>
+
       </div>
   )
 }
